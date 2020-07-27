dd5a91b620cf32c3369daab982ba09f174ec6535
AlexBizPlus <alexeynejnov@gmail.com>
2020-07-27 14:02:51 +0700

    Add mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,69 @@
-// Include gulp
 var gulp = require('gulp'),
-  // Include Our Plugins
+  plumber = require('gulp-plumber'),
+  sourcemap = require('gulp-sourcemaps'),
+  csso = require('gulp-csso'),
+  rename = require('gulp-rename'),
   less = require('gulp-less'),
   postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  del = require('del'),
+  server = require('browser-sync').create(),
+  svgmin = require('gulp-svgmin'),
+  svgstore = require("gulp-svgstore");
 
 gulp.task('css', function () {
-  gulp
-    .src('./less/main.less')
-    .pipe(less({ strictMath: true }))
+  return gulp.src('source/less/style.less')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
     .pipe(postcss([
-      autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
+      autoprefixer()
     ]))
-    .pipe(gulp.dest('./css'));
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('source/css'))
+    .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
+
+gulp.task('normalize', function () {
+  return gulp.src('node_modules/normalize.css/normalize.css')
+    .pipe(gulp.dest('source/css'))
 });
 
-gulp.task('default', ['css', 'watch']);
+gulp.task('svgminimize', function () {
+  return gulp.src('source/img/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sign_*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+// gulp.task('watch', function () {
+//   gulp.watch('./less/**/*.less', ['css']);
+// });
+
+// gulp.task('default', ['css', 'watch']);
+
+gulp.task('server', function () {
+  server.init({
+    server: 'source/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/less/**/*.less', gulp.series('css'));
+  gulp.watch('source/*.html').on('change', server.reload);
+});
+
+gulp.task('start', gulp.series('normalize', 'css', 'server'));
